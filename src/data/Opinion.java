6738775fd3e5415e64ab65fb31a36e760b09ed9e
Dan <36653211+Danixcviii@users.noreply.github.com>
2018-10-09 10:20:21 -0500

    Update Opinion.java

--- src/data/Opinion.java
+++ src/data/Opinion.java
@@ -1,11 +1,20 @@
 package data;
 
 import java.time.LocalDate;
+import java.util.Scanner;
+import java.lang.Integer;
 
 public class Opinion{
   private String author;
   private int starts;
   private LocalDate date;
   private String text;
+  
+  public Opinion(String line){
+   Scanner sc=new Scanner(line);
+    author=sc.nextLine();
+    starts=Integer.parseInt(sc.nextLine());
+    sc.close();
+  }
  
 }
