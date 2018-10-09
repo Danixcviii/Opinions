f480c9328776e4a3319cf95c4827e60e8c034b01
Dan <36653211+Danixcviii@users.noreply.github.com>
2018-10-09 10:14:01 -0500

    Create Tokenizer.java

---
+++ src/bussines/Tokenizer.java
@@ -0,0 +1,10 @@
+package bussines;
+
+import java.util.*;
+
+public class Tokenizer{
+  
+  private TreeMap<String,Integer> lexicon=new TreeMap<String,Integer>();
+  private TreeSet<String> stopWords=new TreeSet<String>();
+
+}
