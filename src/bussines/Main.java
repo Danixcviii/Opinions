b2af53d6da90b54d6b3f078a1023badfbbf3881d
Dan <36653211+Danixcviii@users.noreply.github.com>
2018-10-09 10:33:30 -0500

    Update Main.java

--- src/bussines/Main.java
+++ src/bussines/Main.java
@@ -3,6 +3,7 @@ package bussines;
 public class Main{
  
  public static void main(String[] args){
- 
+  OpinionManager manager=new OpinionManager();
+   manager.readOpinions("dataOpinions.txt");
  }
 }
