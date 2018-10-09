1a6b724addec2f7a028dbae894f6e4fe57d7fe32
Dan <36653211+Danixcviii@users.noreply.github.com>
2018-10-09 10:08:45 -0500

    Update OpinionManager.java

--- src/bussines/OpinionManager.java
+++ src/bussines/OpinionManager.java
@@ -1,5 +1,14 @@
 package bussines;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class OpinionManager{
+ 
+  private String product;
+  private LocalDate startDate;
+  private LocalDate endDate;
+  private ArrayList<Opinion> opinions=new ArrayList<opinion>();
+  
  
 }
